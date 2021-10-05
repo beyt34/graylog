6b7181d88f8e5c9b4628bd060b854972caeaf755
Beytullah Taskin <beytullah.taskin@arvato-scs.com>
2021-10-05 22:25:35 +0300

    sample-api

---
+++ Graylog.SampleApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Graylog.SampleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("WeatherForecast Constructor....");
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var array = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _logger.LogInformation(array[0].SerializeToJson());
+
+            return array;
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static string SerializeToJson<T>(this T serialize) {
+            try {
+                return JsonConvert.SerializeObject(serialize);
+            } catch {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ Graylog.SampleApi/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+using Gelf.Extensions.Logging;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Graylog.SampleApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging((context, builder) => builder.AddGelf(options => {
+                    // Optional customization applied on top of settings in Logging:GELF configuration section.
+                    options.LogSource = context.HostingEnvironment.ApplicationName;
+                    options.AdditionalFields["machine_name"] = Environment.MachineName;
+                    options.AdditionalFields["app_version"] = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+                }));
+        }
+    }
+}
